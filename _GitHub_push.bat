6bfdaff7d84c90957ce0bdc89807259b29b7814f
Tylor Bunting <buntoss@gmail.com>
2020-04-10 23:04:11 +1000

    Updating data

--- _GitHub_push.bat
+++ _GitHub_push.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\R\R-3.6.1\bin\Rscript.exe" C:\Users\tbun2893\Documents\GitHub\COVID19_Analysis\_GitHub_push.R
+"C:\Program Files\R\R-3.6.1\bin\Rscript.exe" C:\Users\tbun2893\Documents\GitHub\COVID19_Analysis\_GitHub_push.R | clip
